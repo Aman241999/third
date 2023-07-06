f436cb0658dfb991e3b51c2525f0e1ea5a240a45
Aman241999 <amansinghania453>
2023-07-06 09:03:52 +0530

    helloworld

---
+++ app.js
@@ -0,0 +1,7 @@
+let item=document.getElementsByClassName("list-group-item");
+item[2].style.fontWeight="bold";
+let items=document.getElementsByTagName("li");
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.color="red";
+}
